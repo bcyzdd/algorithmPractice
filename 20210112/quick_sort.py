62ddef581fe59e0ed0e7a68a7c0775c16864ae77
卜昌义 <buchangyi@yxqiche.com>
2021-01-12 20:51:16 +0800

    快速排序备注分析

--- 20210112/quick_sort.py
+++ 20210112/quick_sort.py
@@ -8,26 +8,33 @@
 
 
 def quick_sort(data, left, right):
+    """
+    
+    :param data:
+    :param left:
+    :param right:
+    :return:
+    """
     if left < right:
         mid = partition(data, left, right)
-        quick_sort(data, left, mid - 1)
-        quick_sort(data, mid + 1, right)
+        quick_sort(data, left, mid - 1) # 对基准数前面进行排序
+        quick_sort(data, mid + 1, right)  # 对基准数前面进行排序
 
 
 def partition(data, left, right):
-    tmp = data[left]
+    tmp = data[left] # 随机选择的基准数，从最左边开始选择
     
     while left < right:
-        while left < right and data[right] >= tmp:
-            right -= 1
-        data[left] = data[right]
+        while left < right and data[right] >= tmp: # 右边的数比基准数大
+            right -= 1 # 保留该数，然后索引指针左移
+        data[left] = data[right] # 否则此时右边比基数小，则将该数据放在基准位置
         
-        while left < right and data[left] <= tmp:
-            left += 1
-        data[right] = data[left]
+        while left < right and data[left] <= tmp: # 左边的数比基准小
+            left += 1  # 此时保持该数位置不变，索引前移
+        data[right] = data[left]  # 否则此时左边的数比基数大，则将该数放在右边
     
-    data[left] = tmp
-    return left
+    data[left] = tmp  # 最后基准数放回中间
+    return left # 返回基准数位置
 
 
 if __name__ == '__main__':
