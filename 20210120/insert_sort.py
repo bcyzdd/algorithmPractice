951262c1b63fabdc0b0b2fa60e3d96824b7ea6e0
卜昌义 <buchangyi@yxqiche.com>
2021-01-22 19:09:22 +0800

    插入排序

---
+++ 20210120/insert_sort.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/1/22 18:58
+# @Author  : bcy
+# @Email   : buchangyi@yxqiche.com
+# @File    : insert_sort.py
+# @Software: PyCharm
+
+
+def insert_sort(data):
+    for i in range(1, len(data)):
+        tmp = data[i]
+        for j in range(i, -1, -1):
+            if tmp < data[j - 1]:
+                data[j] = data[j - 1]
+            else:
+                break
+        data[j] = tmp
+    return i
+
+
+if __name__ == '__main__':
+    import random
+    
+    data= list(range(30))
+    random.shuffle(data)
+    print('pre',data)
+    num = insert_sort(data)
+    print('after',data)
+    print('执行了{}次'.format(num))
